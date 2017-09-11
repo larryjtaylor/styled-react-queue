061e0a6cb8b71bdd2bc5c061b6d388c494856b39
Max Scher <maxobaxo@gmail.com>
2017-09-10 22:18:17 -0700

    implement use of props passed down to child component

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Ticket(props) {
   return (
     <div>
-      <h3>3A - Thato & Haley</h3>
-      <p><em>Firebase won't save record</em></p>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
       <hr/>
     </div>
   );
 }
+
+Ticket.propTypes = {
+  names: PropTypes.string,
+  location: PropTypes.string,
+  issue: PropTypes.string;
+};
 
 export default Ticket;
