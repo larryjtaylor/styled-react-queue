91d24b64881a40af2f2d6f4d85cb8a8131df326d
Max Scher <maxobaxo@gmail.com>
2017-09-10 21:36:09 -0700

    move bulk of code from index.jsx to App component, and render <App/> in ReactDOM method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function App(props) {
-  return ();
+  return (
+    <div>
+      <h1>Help Queue</h1>
+      <h2>{new Date().toLocaleTimeString()}</h2>
+      <h3>3a</h3>
+      <h3>Thato and Haley</h3>
+      <p><em>Firebase won't save record</em></p>
+      <hr/>
+    </div>
+  );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
 ReactDOM.render(
-<div>
-  <h1>Help Queue</h1>
-  <h2>{new Date().toLocaleTimeString()}</h2>
-  <h3>3a</h3>
-  <h3>Thato and Haley</h3>
-  <p><em>Firebase won't save record</em></p>
-</div>,
+  <App/>,
   document.getElementById('react-app-root')
 );
