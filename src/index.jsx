6d97fe0de183d8f09b0b91efc1e51b202c3605ca
Max Scher <maxobaxo@gmail.com>
2017-09-10 17:45:17 -0700

    refactor JavaScript into JSX

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+
+
+ReactDOM.render(
+<div>
+  <h1>Help Queue</h1>
+  <h2>{new Date().toLocaleTimeString()}</h2>
+  <h3>3a</h3>
+  <h3>Thato and Haley</h3>
+  <p><em>Firebase won't save record</em></p>
+</div>,
+  document.getElementById('react-app-root')
+);
