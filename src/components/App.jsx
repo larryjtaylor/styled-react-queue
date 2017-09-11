9dfabd96483b4e2d5e30b19ff94164caa840f46b
Max Scher <maxobaxo@gmail.com>
2017-09-10 21:54:19 -0700

    add child components: TicketList, Ticket, and Header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
+import TicketList from "./TicketList";
+import Header from "./Header";
 
 function App(props) {
   return (
     <div>
-      <h1>Help Queue</h1>
-      <h2>{new Date().toLocaleTimeString()}</h2>
-      <h3>3a</h3>
-      <h3>Thato and Haley</h3>
-      <p><em>Firebase won't save record</em></p>
-      <hr/>
+      <Header/>
+      <TicketList />
+      {/* <h2>{new Date().toLocaleTimeString()}</h2> */}
     </div>
   );
 }
---
+++ src/components/Ticket.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Ticket(props) {
+  return (
+    <div>
+      <h3>3A - Thato & Haley</h3>
+      <p><em>Firebase won't save record</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default Ticket;
