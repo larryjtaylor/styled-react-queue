4e0bf77f82992300f9b094e8b12f379b5f331562
Max Scher <maxobaxo@gmail.com>
2017-09-10 17:32:56 -0700

    add elements to virtualDOM to establish a help queue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-function liveTime() {
-  var heading = React.createElement('h1', {}, 'Help Queue');
-  var app = React.createElement('div', {}, heading);
+var heading = React.createElement('h1', {}, 'Help Queue');
+var ticketLocation = React.createElement('h3', {}, '3a');
+var ticketNames = React.createElement('h3', {}, 'Thato and Haley');
+var ticketIssue = React.createElement('h3', {}, "Firebase won't save record");
+var app = React.createElement('div', {}, heading, ticketLocation, ticketNames, ticketIssue);
 
-  ReactDOM.render(
-    app,
-    document.getElementById('react-app-root')
-  );
-}
+ReactDOM.render(
+  app,
+  document.getElementById('react-app-root')
+);
