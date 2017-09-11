4bf937c2903a7e8828caa48b285ae980fa5e8523
Max Scher <maxobaxo@gmail.com>
2017-09-10 19:58:32 -0700

    add code for babel-loader to handle transformation

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,22 @@ module.exports = {
 
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            "es2015",
+            "react"
+          ] 
+        }
+      }
+    ]
   }
 
 };
