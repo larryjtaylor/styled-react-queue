c8422a97c52c4e5887d090a1c347bf25fb102315
Max Scher <maxobaxo@gmail.com>
2017-09-11 23:12:32 -0700

    bind handleDisplayingNewTicketForm() to instances of NewTicketControl component

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -5,10 +5,12 @@ class NewTicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {formVisibleOnPage: false};
+    this.handleDisplayingNewTicketForm = this.handleDisplayingNewTicketForm.bind(this);
   }
 
   handleDisplayingNewTicketForm(event) {
     console.log("New ticket button was clicked!");
+    this.setState({formVisibleOnPage: true});
   }
 
   render() {
