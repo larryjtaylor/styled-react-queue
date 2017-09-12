1c13630b9332d90ad7ba480e5edb475069679d64
Max Scher <maxobaxo@gmail.com>
2017-09-11 23:55:42 -0700

    add refs to NewTicketForm

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -11,14 +11,17 @@ class NewTicketForm extends React.Component {
       <div>
         <form onSubmit={this.handleNewTicketFormSubmission}>
           <input
+            ref="_names"
             type="text"
             id="names"
             placeholder="Pair Names"/>
           <input
+            ref="_location"
             type="text"
             id="location"
             placeholder="Location"/>
           <textarea
+            ref="_issue"
             id="issue"
             placeholder="Describe your issue."/>
           <button type="submit">Help!</button>
