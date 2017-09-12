5915a36ce2a3f76c7551594e750a88c1aede8408
Max Scher <maxobaxo@gmail.com>
2017-09-11 23:25:23 -0700

    create array of multiple tickets instead of hard-coding just one

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Ticket from "./Ticket";
 
+var masterTicketList = [
+  {
+    names: "Thato & Haley",
+    location: "3A",
+    issue: "Firebase won't save record. Halp."
+  },
+  {
+    names: "Sleater & Kinney",
+    location: "4B",
+    issue: "Fox image not displaying on page, can only see duck?"
+  },
+  {
+    names: "Imani & Jacob",
+    location: "9F",
+    issue: "Donkey picture not displaying on hover in Zoology app. :("
+  }
+];
+
 function TicketList(props) {
   return (
     <Ticket
       location="3a"
-      names="Thato & Haley"
+      names=""
       issue="Firebase won't save record"
     />
   );
