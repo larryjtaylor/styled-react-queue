6238c405316b01d138ade1a50f23f5bc6d30109d
Max Scher and Nathan Stewart <max@>
2017-09-25 08:24:46 -0700

    create app with middleware to add objects to local storage

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-import { createStore } from 'redux';
 import reducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
-const store = createStore(reducer);
+import { createStore, applyMiddleware } from 'redux';
+import middlewareLogger from './middleware/middleware-logger';
+import persistDataLocally from './middleware/persist-local-storage-data';
+
+const store = createStore(reducer, applyMiddleware(middlewareLogger, persistDataLocally));
 
 ReactDOM.render(
   <Provider store={store}>
